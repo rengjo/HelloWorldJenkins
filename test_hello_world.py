86999ee2bac3e3c2d855b7ac2cf5e0f2d8c82d5f
rengjo <139120743+rengjo@users.noreply.github.com>
2024-02-10 16:47:53 +0100

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,4 +1,4 @@
-rom hello_world import say_hello
+from hello_world import say_hello
 
 def test_hello_world():
     assert say_hello() == "Hello, World! Irena Gjorgijeva Hausaufgabe am 09.02.2024 Jenkins mit WSL und Python"
