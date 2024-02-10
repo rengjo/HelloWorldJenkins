dc8925c1bcafa26772710592446b3f0a9408f42f
rengjo <139120743+rengjo@users.noreply.github.com>
2024-02-10 16:42:49 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,10 @@
-# hello_world.py
+"""
+simple function to print "Hello, World! Irena Gjorgijeva Hausaufgabe am 09.02.2024 Jenkins mit WSL und Python".
+"""
 
-def hello_world():
-    return "Hello, World! Irena Gjorgijeva Hausaufgabe am 09.02.2024 Jenkins mit WSL und Python"
+def say_hello():
+
+    print("Hello, World! Irena Gjorgijeva Hausaufgabe am 09.02.2024 Jenkins mit WSL und Python")
+
+if __name__ == "__main__":
+    say_hello()
